9a58e3460a92e15821daae6ff15527d53742815e
stihan01 <stihan01@gmail.com>
2024-02-27 11:14:12 +0100

    added historic stats for cozy games

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+
+
+
+data = pd.read_excel('stats(1).xlsx')
+
 
 app = Dash(__name__)
 
 
+
 app.layout = html.Div([
-    html.H4('Interactive color selection with simple Dash example'),
-    html.P("Select color:"),
-    dcc.Dropdown(
-        id="dropdown",
-        options=['Gold', 'MediumTurquoise', 'LightGreen'],
-        value='Gold',
-        clearable=False,
-    ),
-    dcc.Graph(id="graph"),
+    html.H4('The rise of cozy games'),
+    html.P('All of these genres can be seen as cozy, choose which ones you want to include!'),
+    dcc.Checklist(
+        options= [{'label':'Cozy','value':'cozy'},
+                  {'label':'Wholesome','value':'wholesome'},
+                  {'label':'Relaxing','value':'relaxing'}],
+        value=['cozy'],
+        id='checklist'),
+        dcc.Graph(id='graph')
 ])
 
 
 @app.callback(
     Output("graph", "figure"), 
-    Input("dropdown", "value"))
-def display_color(color):
-    fig = go.Figure(
-        data=go.Bar(y=[2, 3, 1], # replace with your own data source
-                    marker_color=color))
+    Input("checklist", "value"))
+def display_color(selected_genres):
+    fig = px.bar(data, x='år',y=selected_genres, text_auto=True)
+
     return fig
+
+
+
+
 
 
 app.run_server(debug=True)

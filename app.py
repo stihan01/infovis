f96b4193f2b6ce1d97127c88142f4dd8addb3f82
Paulina Palmberg <PaulinaPalmberg@users.noreply.github.com>
2024-02-28 17:01:46 +0100

    pie charts working

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import plotly.express as px
 import pandas as pd
 from flask.helpers import get_root_path
@@ -7,10 +8,36 @@ from flask.helpers import get_root_path
 
 
 data = pd.read_excel('stats(2).xlsx')
+data_US = pd.read_excel('users_USA.xlsx')
+data_EU = pd.read_excel('users_EU.xlsx')
 
 
 app = Dash(__name__)
 
+
+labels = ["Men", "Women"]
+values_US_2020 = [59, 41]
+values_US_2021 = [55, 45]
+values_US_2022 = [52, 48]
+
+values_EU_2020 = [59, 41]
+values_EU_2021 = [55, 45]
+values_EU_2022 = [52, 48]
+
+fig_US_20 = px.pie(data_US, values=values_US_2020, names=labels, title="2020", width = 300)
+fig_US_21 = px.pie(data_US, values=values_US_2021, names=labels, title="2021", width = 300)
+fig_US_22 = px.pie(data_US, values=values_US_2022, names=labels, title="2022", width = 325)
+
+fig_US_20.update_layout(showlegend=False)
+fig_US_21.update_layout(showlegend=False)
+
+fig_EU_20 = px.pie(data_US, values=values_EU_2020, names=labels, title="2020", width = 300)
+fig_EU_21 = px.pie(data_US, values=values_EU_2021, names=labels, title="2021", width = 300)
+fig_EU_22 = px.pie(data_US, values=values_EU_2022, names=labels, title="2022", width = 300)
+
+fig_EU_20.update_layout(showlegend=False)
+fig_EU_21.update_layout(showlegend=False)
+fig_EU_22.update_layout(showlegend=False)
 
 
 app.layout = html.Div([
@@ -20,7 +47,32 @@ app.layout = html.Div([
         options= ['Cozy','Wholesome','Relaxing'],
         value=['Cozy'],
         id='checklist'),
-        dcc.Graph(id='graph')
+        dcc.Graph(id='graph'),
+    html.H3("USA Players:"),
+    html.Div(className="row", children=[
+
+        dcc.Graph(id='US20',figure=fig_US_20, style={
+            "display": "inline-block"
+        }),
+        dcc.Graph(id='US21',figure=fig_US_21, style={
+            "display": "inline-block"
+        }),
+        dcc.Graph(id='US22',figure=fig_US_22, style={
+            "display": "inline-block"
+        }),
+        html.H3("Europe Players:"),
+        dcc.Graph(id='EU20',figure=fig_EU_20, style={
+            "display": "inline-block"
+        }),
+        dcc.Graph(id='EU21',figure=fig_EU_21, style={
+            "display": "inline-block"
+        }),
+        dcc.Graph(id='EU22',figure=fig_EU_22, style={
+            "display": "inline-block"
+        })
+    ])
+
+    
 ])
 
 

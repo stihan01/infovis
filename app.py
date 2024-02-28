af1931dacc8ef076bf30553d597e3c45673f07bd
stihan01 <stihan01@gmail.com>
2024-02-28 14:04:52 +0100

    made things pretty

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from dash import Dash, dcc, html, Input, Output
 import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
+from flask.helpers import get_root_path
 
 
 
-data = pd.read_excel('stats(1).xlsx')
+data = pd.read_excel('stats(2).xlsx')
 
 
 app = Dash(__name__)
@@ -13,13 +14,11 @@ app = Dash(__name__)
 
 
 app.layout = html.Div([
-    html.H4('The rise of cozy games'),
+    html.H2('The rise of cozy games'),
     html.P('All of these genres can be seen as cozy, choose which ones you want to include!'),
     dcc.Checklist(
-        options= [{'label':'Cozy','value':'cozy'},
-                  {'label':'Wholesome','value':'wholesome'},
-                  {'label':'Relaxing','value':'relaxing'}],
-        value=['cozy'],
+        options= ['Cozy','Wholesome','Relaxing'],
+        value=['Cozy'],
         id='checklist'),
         dcc.Graph(id='graph')
 ])
@@ -29,11 +28,24 @@ app.layout = html.Div([
     Output("graph", "figure"), 
     Input("checklist", "value"))
 def display_color(selected_genres):
-    fig = px.bar(data, x='år',y=selected_genres, text_auto=True)
+    fig = px.bar(
+        data, x='år',
+        y=selected_genres, 
+        text_auto=True, 
+        title='Number of cozy games on steam over time',
+        labels={'år':'Year','value':'Number of games in this category','variable':'Category'},
+        color_discrete_sequence=px.colors.qualitative.Pastel
+        )
+    fig.update_layout(
+        font_family="Poppins"
+    )
+    
+        
 
     return fig
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 

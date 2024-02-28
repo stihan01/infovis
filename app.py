93059a687c797d5daae4be898c7ee7a8b806a1c4
stihan01 <stihan01@gmail.com>
2024-02-28 16:23:21 +0100

    made thinks even prettier

--- app.py
+++ app.py
@@ -11,38 +11,47 @@ data = pd.read_excel('stats(2).xlsx')
 
 app = Dash(__name__)
 
-
-
-app.layout = html.Div([
-    html.H2('The rise of cozy games'),
-    html.P('All of these genres can be seen as cozy, choose which ones you want to include!'),
-    dcc.Checklist(
-        options= ['Cozy','Wholesome','Relaxing'],
-        value=['Cozy'],
-        id='checklist'),
-        dcc.Graph(id='graph')
-])
-
-
-@app.callback(
-    Output("graph", "figure"), 
-    Input("checklist", "value"))
-def display_color(selected_genres):
-    fig = px.bar(
+fig = px.bar(
         data, x='år',
-        y=selected_genres, 
+        y=['Cozy', 'Wholesome', 'Relaxing'], 
         text_auto=True, 
         title='Number of cozy games on steam over time',
         labels={'år':'Year','value':'Number of games in this category','variable':'Category'},
         color_discrete_sequence=px.colors.qualitative.Pastel
         )
-    fig.update_layout(
-        font_family="Poppins"
-    )
+fig.update_layout(
+    font_family="Poppins"
+)
+
+#dcc.Checklist(
+            #options= ['Cozy','Wholesome','Relaxing'],
+            #value=['Cozy'],
+            #id='checklist'),
+
+
+
+app.layout = html.Div([
+    html.Div(className='cosmo', children = [
+        html.Img(src=r'assets/cosmopolitan.png', alt='image')]),
+    html.Div(className='main-site', children= [
+        html.H1('The rise of cozy games', className='big-title'),
+        html.P('All of these genres can be seen as cozy, choose which ones you want to include!'),
+        dcc.Graph(id='graph', figure=fig)
+        ])
+])
+
+
+
+# @app.callback(
+#     Output("graph", "figure"), 
+#     Input("checklist", "value"))
+# def display_color(selected_genres):
+    
     
         
 
-    return fig
+#     return fig
+
 
 if __name__ == '__main__':
     app.run(debug=True)
